8b293e62e4a158b20199ae2690e7358e41e4c11e
edilton-bs <ediltonbs5@gmail.com>
2025-05-20 13:48:10 -0300

    voltando somente filmes com trailer

--- landing_page/app.py
+++ landing_page/app.py
@@ -8,7 +8,7 @@ import os
 app = Flask(__name__)
 
 # Carregar o CSV 
-df = pd.read_csv('static/movies_final.csv')  # Ajuste o caminho conforme necessário
+df = pd.read_csv('static/filmes_tmdb.csv')  # Ajuste o caminho conforme necessário
 
 # dar Gêneros aleatórios para os filmes
 # generos = ['Ação', 'Aventura', 'Comédia', 'Drama', 'Terror', 'Ficção Científica', 'Romance', 'Animação']
